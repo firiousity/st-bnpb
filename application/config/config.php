b6d3378a29dc847041a38b71c14785b74e9aea55
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-07-30 11:15:57 +0700

    add header.php which to load assets

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/st-bnpb/index.php/';
+$config['base_url'] = 'http://localhost/st-bnpb/';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/views/header.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MNurilmanBaehaqi
+ * Date: 7/30/2018
+ * Time: 11:13 AM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Material Design Bootstrap</title>
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+	<!-- Bootstrap core CSS -->
+	<link href="<?php echo base_url('assets/css/bootstrap.css')?>" rel="stylesheet">
+	<!-- Material Design Bootstrap -->
+	<link href="<?php echo base_url('assets/css/mdb.css')?>" rel="stylesheet">
+	<!-- Your custom styles (optional) -->
+	<link href="<?php echo base_url('assets/css/style.css')?>" rel="stylesheet">
+</head>
+
