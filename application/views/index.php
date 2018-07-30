eb9ffe1f717048c6c7001aa8b99b5e05633ee222
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-07-30 10:35:32 +0700

    add base configuration such as name of db, route, and base_url

--- application/views/index.php
+++ application/views/index.php
@@ -3,7 +3,7 @@
 		<title>Surat Tugas</title>
 		<meta charset="utf-8">
 		<meta name="description" content="Administrasi surat tugas karyawan.">
-		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="../../assets/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
 	</head>
 </html>
