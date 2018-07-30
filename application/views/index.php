be1bd0ab52306a556940b36183ac101e98a74f5d
firiousity <firly.achmad@gmail.com>
2018-07-30 11:06:48 +0700

    add php base_url index

--- application/views/index.php
+++ application/views/index.php
@@ -9,11 +9,11 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url('assets/css/bootstrap-min.css')?> css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/bootstrap-min.css')?>" rel="stylesheet">
     <!-- Material Design Bootstrap -->
-    <link href="<?php echo base_url('assets/css/bootstrap.css')?> css/mdb.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/bootstrap.css')?>" rel="stylesheet">
     <!-- Your custom styles (optional) -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/style.css')?>" rel="stylesheet">
 </head>
 
 <body>
@@ -32,13 +32,13 @@
 
     <!-- SCRIPTS -->
     <!-- JQuery -->
-    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-3.2.1.min.js')?>"></script>
     <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/css/popper.min.js')?>"></script>
     <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/css/bootstrap.min.js')?>"></script>
     <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/css//mdb.min.js')?>"></script>
 </body>
 
 </html>
