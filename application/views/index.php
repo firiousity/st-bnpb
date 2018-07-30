7be7b842bba227ade9749bc568eb9fc601bbb6b9
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-07-30 11:15:29 +0700

    add header.php which to load assets

--- application/views/index.php
+++ application/views/index.php
@@ -1,25 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Material Design Bootstrap</title>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
-    <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url('assets/css/bootstrap-min.css')?>" rel="stylesheet">
-    <!-- Material Design Bootstrap -->
-    <link href="<?php echo base_url('assets/css/bootstrap.css')?>" rel="stylesheet">
-    <!-- Your custom styles (optional) -->
-    <link href="<?php echo base_url('assets/css/style.css')?>" rel="stylesheet">
-</head>
 
 <body>
+<?php echo base_url('assets/css/style.css')?>
 
     <!-- Start your project here-->
-    <div style="height: 100vh">
+    <div style="height: 100px">
         <div class="flex-center flex-column">
             <h1 class="animated fadeIn mb-2">Material Design for Bootstrap</h1>
 
