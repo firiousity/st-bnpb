416abb061dd2dc4f6822cd48830ea7dc03fe81ff
Kelik Is Cahyanto <kelikisc@gmail.com>
2018-07-30 11:45:36 +0700

    script

--- application/views/header.php
+++ application/views/header.php
@@ -10,6 +10,7 @@
 <html lang="en">
 
 <head>
+	<link rel="shortcut icon"  href="<?php echo base_url('assets/img/logo.png')?>">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -22,5 +23,15 @@
 	<link href="<?php echo base_url('assets/css/mdb.css')?>" rel="stylesheet">
 	<!-- Your custom styles (optional) -->
 	<link href="<?php echo base_url('assets/css/style.css')?>" rel="stylesheet">
+
+	<!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-3.2.1.min.js')?>"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="<?php echo base_url('assets/css/popper.min.js')?>"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="<?php echo base_url('assets/css/bootstrap.min.js')?>"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="<?php echo base_url('assets/css//mdb.min.js')?>"></script>
 </head>
 
--- application/views/index.php
+++ application/views/index.php
@@ -4,26 +4,48 @@ require_once ('header.php');
 <body>
 
     <!-- Start your project here-->
-    <div style="height: 100px">
+    <div style="height: 100vh">
         <div class="flex-center flex-column">
+
+<!-- Default form login -->
+<form class="text-center border border-light p-5">
+
+    <p class="h4 mb-4">Sign in</p>
+
+    <!-- Email -->
+    <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+    <!-- Password -->
+    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
+
+    <div class="d-flex justify-content-around">
+        <div>
+            <!-- Remember me -->
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+                <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+            </div>
+        </div>
+        <div>
+            <!-- Forgot password -->
+            <a href="">Forgot password?</a>
+        </div>
+    </div>
+
+    <!-- Sign in button -->
+    <button class="btn btn-info btn-block my-4" type="submit">Sign in</button>
+
+</form>
+<!-- Default form login -->
             <h1 class="animated fadeIn mb-2">Material Design for Bootstrap</h1>
 
             <h5 class="animated fadeIn mb-1">Thank you for using our product. We're glad you're with us.</h5>
 
             <p class="animated fadeIn text-muted">MDB Team</p>
         </div>
-    </div>
-    <!-- /Start your project here-->
 
-    <!-- SCRIPTS -->
-    <!-- JQuery -->
-    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-3.2.1.min.js')?>"></script>
-    <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="<?php echo base_url('assets/css/popper.min.js')?>"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="<?php echo base_url('assets/css/bootstrap.min.js')?>"></script>
-    <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="<?php echo base_url('assets/css//mdb.min.js')?>"></script>
+    </div>
+    
 </body>
 
 </html>
