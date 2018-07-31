778eca2cc07a041e05f341818b1c68584a75d076
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-07-31 15:30:44 +0700

    placing image background only in beranda

--- application/views/header.php
+++ application/views/header.php
@@ -43,24 +43,7 @@
 	<link href="<?php echo base_url('assets/css/multiselect/multi-select.dist.css')?>" rel="stylesheet">
 	<!-- Your custom styles (optional) -->
 	<link href="<?php echo base_url('assets/css/style.css')?>"  type="text/css" rel="stylesheet">
-            <!-- Intro Section -->
-            <div class="view hm-black-light jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url(https://mdbootstrap.com/img/Photos/Others/img%20%2848%29.jpg); height: 100%">
-                <div class="full-bg-img">
-                    <div class="container flex-center">
-                        <div class="row pt-5 mt-3">
-                            <div class="col-md-12">
-                                <div class="text-center">
-                                    <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-3 wow fadeInDown" data-wow-delay="0.3s"><strong>Minimalist intro</strong></h1>
-                                    <hr class="hr-light mt-4 wow fadeInDown" data-wow-delay="0.4s">
-                                    <h5 class="text-uppercase mb-5 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>Photography & design</strong></h5>
-                                    <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">portfolio</a>
-                                    <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">About me</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>	
+
 	<!-- SCRIPTS -->
     <!-- JQuery -->
 	<script
