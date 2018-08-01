34ac888b4438a3a42288c92f1af816255ec4accc
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-08-01 13:17:39 +0700

    integrating with fpdf

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -99,6 +99,92 @@ class Home extends CI_Controller {
 		//$this->db->insert('surat_dinas', $data);
 	}
 
+	function test_pdf($id) {
+//		$this->load->view('navbar');
+//		$this->load->view('header');
+//		$this->load->view('test_pdf');
+//		$this->load->view('footer');
+
+		$tab = "\t \t \t \t \t";
+		$surat_tugas = $this->db->get_where('surat_dinas', array('id' => $id))->result();
+		//$surat_tugas = json_encode($surat_tugas);
+		$pdf = new FPDF('p','mm','A4');
+
+		// membuat halaman baru
+		$pdf->AddPage();
+		$pdf->SetLeftMargin(20);
+		$pdf->SetRightMargin(20);
+		// setting jenis font yang akan digunakan
+		$pdf->SetFont('Arial','B',14);
+		// mencetak string
+		$pdf->Cell(0,6,"",0,1,'C');
+		$pdf->Ln();
+		$pdf->Cell(0,6,"",0,1,'C');
+		$pdf->Ln();
+		$pdf->Ln();
+		$pdf->Cell(0,6,"",0,1,'C');
+		$pdf->Ln();
+		$pdf->Cell(0,3,'SURAT TUGAS',0,1,'C');
+		$pdf->SetFont('Arial','',12);
+		$pdf->Cell(0,10,"NOMOR: ".$surat_tugas['0']->nomor,0,1,'C');
+		// Memberikan space kebawah agar tidak terlalu rapat
+		$pdf->Cell(10,7,'',0,1);
+		$pdf->SetFont('Arial','',10);
+		$pdf->Cell(25,6,'Menimbang',0,0);
+		$pdf->Cell(5,6,':',0,0);
+		$pdf->MultiCell(0,6,"Dalam rangka melakukan kegiatan kegiatan di tempat  pada waktu",0,'J');
+		//$pdf->Cell(27,6,'Dalam rangka melakukan kegiatan $kegiatan di $tempat  pada $waktu	',0,0);
+		$pdf->Cell(25,6,'Dasar',0,0);
+		$pdf->Cell(5,6,':',0,0);
+		$pdf->Cell(5,6,'1.',0,0);
+		$pdf->MultiCell(0,6,"Keputusan Presiden Nomor 72 Tahun 2004 tentang Pelaksanaan AnggaranPendapatan dan Belanja Negara;",0,'J');
+		$pdf->Cell(25,6,'',0,0);
+		$pdf->Cell(5,6,'',0,0);
+		$pdf->Cell(5,6,'2.',0,0);
+		$pdf->MultiCell(0,6,"Peraturan Menteri Keuangan Nomor 134/PMK.06/2005 tentang Pedoman Pembayaran dalam Pelaksanan Anggaran Pendapatan dan Belanja Negara;",0,'J');
+		$pdf->Cell(25,6,'',0,0);
+		$pdf->Cell(5,6,'',0,0);
+		$pdf->Cell(5,6,'3.',0,0);
+		$pdf->MultiCell(0,6,"Peraturan Kepala Badan Nasinal Penanggulangan Bencana Nomor 1 tahun 2008 tentang Organisasi dan Tata Kerja Badan Nasional Penanggulangan Bencana",0,'J');
+		$pdf->SetFont('Arial','B',12);
+		$pdf->Cell(0,10,"Memberi tugas",0,1,'C');
+		$pdf->SetFont('Arial','',10);
+		$pdf->Cell(25,6,'Kepada',0,0);
+		$pdf->Cell(5,6,':',0,0);
+		$pdf->MultiCell(0,6,"Daftar Terlampir",0,'L');
+		$pdf->Cell(25,6,'Untuk',0,0);
+		$pdf->Cell(5,6,':',0,0);
+		$pdf->Cell(5,6,'1.',0,0);
+		$pdf->MultiCell(0,6,"Dinas ke var_tempat dalam rangka mendukung kegiatan var_kegiatan tahun var_tahun, pada tanggal var_tgl_mulai s.d var_tgl_akhir;",0,'J');
+		$pdf->Cell(25,6,'',0,0);
+		$pdf->Cell(5,6,'',0,0);
+		$pdf->Cell(5,6,'2.',0,0);
+		$pdf->MultiCell(0,6,"Melaksanakan tugas ini dengan penuh tanggungjawab;",0,'L');
+		$pdf->Cell(25,6,'',0,0);
+		$pdf->Cell(5,6,'',0,0);
+		$pdf->Cell(5,6,'3.',0,0);
+		$pdf->MultiCell(0,6,"Segala biaya yang dikeluarkan untuk tugas tersebut di atas dibebankan kepada DIPA BNPB TA 2018, Pos Kegiatan Melakukan Monitoring dan Evaluasi Teknologi Informasi dan Komunikasi;",0,'J');
+		$pdf->Cell(25,6,'',0,0);
+		$pdf->Cell(5,6,'',0,0);
+		$pdf->Cell(5,6,'2.',0,0);
+		$pdf->MultiCell(0,6,"Apabila terdapat kekeliruan dalam Surat Tugas ini akan dilakukan perbaikan sebagaimana mestinya.",0,'J');
+		$pdf->Ln();
+		$pdf->MultiCell(0,6,"Jakarta, var_tgl-bulan-tahun",0,'R');
+		$pdf->MultiCell(0,6,"Kepala Pusat Data Informasi dan Humas",0,'R');
+		$pdf->Ln();
+		$pdf->Ln();
+		$pdf->Ln();
+		$pdf->SetFont('Arial','B',12);
+		$pdf->MultiCell(0,6,"DR. Sutopo Purwo Nugroho, M.Si, APU	",0,'R');
+
+		$pdf->Output();
+
+	}
+	function ena ($id) {
+		$surat_tugas = $this->db->select('nomor')->get_where('surat_dinas', array('id' => $id))->result();
+		echo $surat_tugas['0']->nomor;
+	}
+
 	function href ($route) {
 		echo "<script>
          	window.location.href='$route';</script>";
