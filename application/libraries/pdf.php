3e2795e5e68a72e432dd20fcd3f6af3b6bb78e56
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-08-01 13:18:51 +0700

    integrating with fpdf

---
+++ application/libraries/pdf.php
@@ -0,0 +1,9 @@
+<?php
+class pdf {
+
+	function __construct() {
+		include_once APPPATH . '/third_party/fpdf/fpdf.php';
+		//include_once APPPATH . '/third_party/fpdf/PDF.php';
+	}
+}
+?>
--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -7,8 +7,12 @@ class Home_model extends CI_Model {
 	}
 
 	function get_nomor() {
+		$this->db->select('id');
 		$this->db->from('surat_dinas');
-		return $query = $this->db->get();
+		$this->db->order_by('id', 'desc');
+		$this->db->limit(1);
+		$query = $this->db->get();
+		return $query->result_array();
 	}
 
 
