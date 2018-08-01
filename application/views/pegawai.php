0047e9001aad534fb5904290d4706ded8ef43fd0
Kelik Is Cahyanto <kelikisc@gmail.com>
2018-08-01 13:52:34 +0700

    modal tambah pegawai

--- application/views/pegawai.php
+++ application/views/pegawai.php
@@ -1,6 +1,47 @@
 <body>
     <section>
+                    <div class="modal fade" id="modalRegisterForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold">Sign up</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body mx-5">
+                <div class="md-form mb-4">
+                    <!-- <i class="fa fa-user prefix grey-text"></i> -->
+                    <input type="text" id="orangeForm-name" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-name">Nama Lengkap</label>
+                </div>
+                <div class="md-form mb-4">
+                    <!-- <i class="fa fa-address-card prefix grey-text"></i> -->
+                    <input type="email" id="orangeForm-email" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-email">NIP</label>
+                </div>
+
+                <div class="md-form mb-4">
+                    <!-- <i class="fa fa-lock prefix grey-text"></i> -->
+                    <input type="password" id="orangeForm-pass" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-pass">Jabatan</label>
+                </div>
+
+                <div class="md-form mb-4">
+                    <!-- <i class="fa fa-lock prefix grey-text"></i> -->
+                    <input type="password" id="orangeForm-pass" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-pass">Golongan</label>
+                </div>
+
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <button class="btn btn-deep-orange">Sign up</button>
+            </div>
+        </div>
+    </div>
+</div>
         <div class="container center">
+
                 <div class="card mb-8">
 
                     <div class="card-body">
@@ -34,13 +75,17 @@
                             } ?>
                       </tbody>
                     </table>
+      
+
+<!-- <div class="text-center">
+    <a href="" class="btn btn-default btn-rounded mb-4" data-toggle="modal" data-target="#modalRegisterForm">Launch Modal Register Form</a>
+</div> -->
                         <div class="row">
                             <div class="col">
                                 <div align="left">
-                                    <a href="#"><button type="button" class="btn btn-indigo btn-md">Tambah Pegawai</button></a>                    
+                                    <a href="#"><button type="button" data-toggle="modal" data-target="#modalRegisterForm" class="btn btn-indigo btn-md">Tambah Pegawai</button></a>                    
                                 </div>
-                
-            </div> 
+                              </div> 
                             <div class="col">
                                 <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-end">
