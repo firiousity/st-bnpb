3bd89e838fa31b705029a55a1b189ccda52708d8
Kelik Is Cahyanto <kelikisc@gmail.com>
2018-08-01 11:52:22 +0700

    halaman2 anggaran

---
+++ application/views/uang_representasi.php
@@ -0,0 +1,98 @@
+<body>
+    <section>
+        <div class="container center">
+                <div class="card mb-8">
+
+                    <div class="card-body">
+                        <p style="font-size: 27px;">Kelola Pegawai</p>
+                        <div align="center">
+                        <table class="table table-hover">
+                          <thead class="mdb-color darken-3 white-text">
+                            <tr>
+                              <th scope="col">Nomer</th>
+                              <th scope="col">Nama</th>
+                              <th scope="col">NIP</th>
+                              <th scope="col">Jabatan</th>
+                              <th scope="col">Golongan</th>
+                              <th scope="col">Hapus</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>Mark Otto</td>
+                              <td>31451517800</td>
+                              <td>Kepala Bidang</td>
+                              <td>III</td>
+                              <td><span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0">Hapus</button></span></td>
+                            </tr>
+                            <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>Kepala Bidang</td>
+                              <td>III</td>
+                              <td><span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0">Hapus</button></span></td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td> Larry the Bird</td>
+                          <td>@twitter</td>
+                          <td>Kepala Bidang</td>
+                            <td>III</td>
+                            <td><span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0">Hapus</button></span></td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td> Larry the Bird</td>
+                          <td>@twitter</td>
+                          <td>Kepala Bidang</td>
+                            <td>III</td>
+                            <td><span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0">Hapus</button></span></td>
+                        </tr>
+
+                      </tbody>
+                    </table>
+                        <div class="row">
+                            <div class="col">
+                                <div align="left">
+                                    <a href="#"><button type="button" class="btn btn-indigo btn-md">Tambah Pegawai</button></a>                    
+                                </div>
+                
+            </div> 
+                            <div class="col">
+                                <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-end">
+        <!--Previous-->
+        <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Previous</a>
+        </li>
+
+        <!--Numbers-->
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+
+        <!--Next-->
+        <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+        </li>
+    </ul>
+</nav>
+                            </div>
+                    </div>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        
+    </div>
+
+</body>
+
+</html>
