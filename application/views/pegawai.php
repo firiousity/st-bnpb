8198bda5a35697fd3bbb06145bb978eb751ed6ee
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-08-01 14:51:30 +0700

    dududu

--- application/views/pegawai.php
+++ application/views/pegawai.php
@@ -9,34 +9,36 @@
 							<span aria-hidden="true">&times;</span>
 						</button>
 					</div>
-					<div class="modal-body mx-5">
-						<div class="md-form mb-4">
-							<!-- <i class="fa fa-user prefix grey-text"></i> -->
-							<input type="text" id="orangeForm-name" class="form-control validate" name="nama">
-							<label data-error="wrong" data-success="right" for="orangeForm-name">Nama Lengkap</label>
-						</div>
-						<div class="md-form mb-4">
-							<!-- <i class="fa fa-address-card prefix grey-text"></i> -->
-							<input type="text" id="orangeForm-email" class="form-control validate" name="nip">
-							<label data-error="wrong" data-success="right" for="orangeForm-email">NIP</label>
-						</div>
+					<form action="" method="post">
+						<div class="modal-body mx-5">
+							<div class="md-form mb-4">
+								<!-- <i class="fa fa-user prefix grey-text"></i> -->
+								<input type="text" id="orangeForm-name" class="form-control validate" name="nama">
+								<label data-error="wrong" data-success="right" for="orangeForm-name">Nama Lengkap</label>
+							</div>
+							<div class="md-form mb-4">
+								<!-- <i class="fa fa-address-card prefix grey-text"></i> -->
+								<input type="text" id="orangeForm-email" class="form-control validate" name="nip">
+								<label data-error="wrong" data-success="right" for="orangeForm-email">NIP</label>
+							</div>
 
-						<div class="md-form mb-4">
-							<!-- <i class="fa fa-lock prefix grey-text"></i> -->
-							<input type="text" id="orangeForm-pass" class="form-control validate" name="jabatan">
-							<label data-error="wrong" data-success="right" for="orangeForm-pass">Jabatan</label>
-						</div>
+							<div class="md-form mb-4">
+								<!-- <i class="fa fa-lock prefix grey-text"></i> -->
+								<input type="text" id="orangeForm-pass" class="form-control validate" name="jabatan">
+								<label data-error="wrong" data-success="right" for="orangeForm-pass">Jabatan</label>
+							</div>
 
-						<div class="md-form mb-4">
-							<!-- <i class="fa fa-lock prefix grey-text"></i> -->
-							<input type="text" id="orangeForm-pass" class="form-control validate" name="gol">
-							<label data-error="wrong" data-success="right" for="orangeForm-pass">Golongan</label>
-						</div>
+							<div class="md-form mb-4">
+								<!-- <i class="fa fa-lock prefix grey-text"></i> -->
+								<input type="text" id="orangeForm-pass" class="form-control validate" name="gol">
+								<label data-error="wrong" data-success="right" for="orangeForm-pass">Golongan</label>
+							</div>
 
-					</div>
-					<div class="modal-footer d-flex justify-content-center">
-						<button class="btn btn-indigo">Tambah</button>
-					</div>
+						</div>
+						<div class="modal-footer d-flex justify-content-center">
+							<button class="btn btn-indigo">Tambah</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
@@ -52,34 +54,36 @@
 							<span aria-hidden="true">&times;</span>
 						</button>
 					</div>
-					<div class="modal-body mx-5">
-						<div class="md-form mb-4">
-							<!-- <i class="fa fa-user prefix grey-text"></i> -->
-							<input type="text" id="orangeForm-name" class="form-control validate" name="nama">
-							<label data-error="wrong" data-success="right" for="orangeForm-name">Nama Lengkap</label>
-						</div>
-						<div class="md-form mb-4">
-							<!-- <i class="fa fa-address-card prefix grey-text"></i> -->
-							<input type="text" id="orangeForm-email" class="form-control validate" name="nip">
-							<label data-error="wrong" data-success="right" for="orangeForm-email">NIP</label>
-						</div>
+					<form action="" method="post">
+						<div class="modal-body mx-5">
+							<div class="md-form mb-4">
+								<!-- <i class="fa fa-user prefix grey-text"></i> -->
+								<input type="text" id="orangeForm-name" class="form-control validate" name="nama">
+								<label data-error="wrong" data-success="right" for="orangeForm-name">Nama Lengkap</label>
+							</div>
+							<div class="md-form mb-4">
+								<!-- <i class="fa fa-address-card prefix grey-text"></i> -->
+								<input type="text" id="orangeForm-email" class="form-control validate" name="nip">
+								<label data-error="wrong" data-success="right" for="orangeForm-email">NIP</label>
+							</div>
 
-						<div class="md-form mb-4">
-							<!-- <i class="fa fa-lock prefix grey-text"></i> -->
-							<input type="text" id="orangeForm-pass" class="form-control validate" name="jabatan">
-							<label data-error="wrong" data-success="right" for="orangeForm-pass">Jabatan</label>
-						</div>
+							<div class="md-form mb-4">
+								<!-- <i class="fa fa-lock prefix grey-text"></i> -->
+								<input type="text" id="orangeForm-pass" class="form-control validate" name="jabatan">
+								<label data-error="wrong" data-success="right" for="orangeForm-pass">Jabatan</label>
+							</div>
 
-						<div class="md-form mb-4">
-							<!-- <i class="fa fa-lock prefix grey-text"></i> -->
-							<input type="text" id="orangeForm-pass" class="form-control validate" name="gol">
-							<label data-error="wrong" data-success="right" for="orangeForm-pass">Golongan</label>
-						</div>
+							<div class="md-form mb-4">
+								<!-- <i class="fa fa-lock prefix grey-text"></i> -->
+								<input type="text" id="orangeForm-pass" class="form-control validate" name="gol">
+								<label data-error="wrong" data-success="right" for="orangeForm-pass">Golongan</label>
+							</div>
 
-					</div>
-					<div class="modal-footer d-flex justify-content-center">
-						<button class="btn btn-indigo">Edit</button>
-					</div>
+						</div>
+						<div class="modal-footer d-flex justify-content-center">
+							<button class="btn btn-indigo">Edit</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
