8fc3f3b1a1f7db7faf03e8e36831fb7734448d2b
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-08-02 15:22:27 +0700

    add function terbilang in page 3

--- application/controllers/C_PDF.php
+++ application/controllers/C_PDF.php
@@ -239,7 +239,7 @@ class C_PDF extends CI_Controller {
 		$pdf->Cell(15,6,'',0,0,'L');
 		$pdf->Cell(5,6,'',0,0,'L');
 		$pdf->Cell(25,6,'Terbilang: ',0,0,'R');
-		$pdf->MultiCell(80,6,'Sembilan juta empat puluh satu ribu rupiah',0,'C');
+		$pdf->MultiCell(90,6,$this->terbilang(1290000)." rupiah",0,'L');
 		//$pdf->MultiCell(50,6,'',0,'L');
 
 		$pdf->Ln();
@@ -295,5 +295,96 @@ class C_PDF extends CI_Controller {
 	function ena ($id) {
 		$surat_tugas = $this->db->select('nomor')->get_where('surat_dinas', array('id' => $id))->result();
 		echo $surat_tugas['0']->nomor;
+	}
+
+
+	function terbilang($bilangan) {
+
+		$angka = array('0','0','0','0','0','0','0','0','0','0',
+			'0','0','0','0','0','0');
+		$kata = array('','satu','dua','tiga','empat','lima',
+			'enam','tujuh','delapan','sembilan');
+		$tingkat = array('','ribu','juta','milyar','triliun');
+
+		$panjang_bilangan = strlen($bilangan);
+
+		/* pengujian panjang bilangan */
+		if ($panjang_bilangan > 15) {
+			$kalimat = "Diluar Batas";
+			return $kalimat;
+		}
+
+		/* mengambil angka-angka yang ada dalam bilangan,
+           dimasukkan ke dalam array */
+		for ($i = 1; $i <= $panjang_bilangan; $i++) {
+			$angka[$i] = substr($bilangan,-($i),1);
+		}
+
+		$i = 1;
+		$j = 0;
+		$kalimat = "";
+
+
+		/* mulai proses iterasi terhadap array angka */
+		while ($i <= $panjang_bilangan) {
+
+			$subkalimat = "";
+			$kata1 = "";
+			$kata2 = "";
+			$kata3 = "";
+
+			/* untuk ratusan */
+			if ($angka[$i+2] != "0") {
+				if ($angka[$i+2] == "1") {
+					$kata1 = "seratus";
+				} else {
+					$kata1 = $kata[$angka[$i+2]] . " ratus";
+				}
+			}
+
+			/* untuk puluhan atau belasan */
+			if ($angka[$i+1] != "0") {
+				if ($angka[$i+1] == "1") {
+					if ($angka[$i] == "0") {
+						$kata2 = "sepuluh";
+					} elseif ($angka[$i] == "1") {
+						$kata2 = "sebelas";
+					} else {
+						$kata2 = $kata[$angka[$i]] . " belas";
+					}
+				} else {
+					$kata2 = $kata[$angka[$i+1]] . " puluh";
+				}
+			}
+
+			/* untuk satuan */
+			if ($angka[$i] != "0") {
+				if ($angka[$i+1] != "1") {
+					$kata3 = $kata[$angka[$i]];
+				}
+			}
+
+			/* pengujian angka apakah tidak nol semua,
+               lalu ditambahkan tingkat */
+			if (($angka[$i] != "0") OR ($angka[$i+1] != "0") OR
+				($angka[$i+2] != "0")) {
+				$subkalimat = "$kata1 $kata2 $kata3 " . $tingkat[$j] . " ";
+			}
+
+			/* gabungkan variabe sub kalimat (untuk satu blok 3 angka)
+               ke variabel kalimat */
+			$kalimat = $subkalimat . $kalimat;
+			$i = $i + 3;
+			$j = $j + 1;
+
+		}
+
+		/* mengganti satu ribu jadi seribu jika diperlukan */
+		if (($angka[5] == "0") AND ($angka[6] == "0")) {
+			$kalimat = str_replace("satu ribu","seribu",$kalimat);
+		}
+
+		return trim($kalimat);
+
 	}
 }
