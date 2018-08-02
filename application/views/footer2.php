57a34e7c22a0e88f22c7b960e33a62c9fbf8c34b
firiousity <firly.achmad@gmail.com>
2018-08-02 15:56:46 +0700

    add link to copyright

--- application/views/footer2.php
+++ application/views/footer2.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="page-footer fixed-bottom font-small unique-color-dark">
-  <div class="footer-copyright py-3" align="center">© 2018 Copyright: Native.
+  <div class="footer-copyright py-3" align="center">© 2018 Copyright: <a href="https://instagram.com/kelikisc">.Native
   </div>
 </footer>
 <!-- Footer -->
