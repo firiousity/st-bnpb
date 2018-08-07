0e308811470ddc6d0d20db6c52fb23bf60f911d0
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-08-07 08:39:26 +0700

    debugging form_dinamis using jquery

--- application/views/dynamic_form.php
+++ application/views/dynamic_form.php
@@ -30,24 +30,30 @@
 <div class="row-fluid">
 	<div class="span6">
 		<form action="#" method="post">
-			<table class="table table-condensed">
+			<table class="table table-responsive	">
 				<thead>
 				<tr>
-					<th width="300px">Jenis Barang</th>
-					<th width="100px">Jumlah</th>
-					<th width="80px"></th>
+					<th>Nama</th>
+					<th>Tempat</th>
+					<th>Tgl Mulai</th>
+					<th>Tgl Akhir</th>
+					<th></th>
 				</tr>
 				</thead>
 				<!--elemet sebagai target append-->
 				<tbody id="itemlist">
 				<tr>
-					<td><input name="jenis_input[0]" class="input-block-level" /></td>
-					<td><input name="jumlah_input[0]" class="input-block-level" /></td>
+					<td><input name="nama_input[0]" class="input-block-level" type="text"/></td>
+					<td><input name="tempat_input[0]" class="input-block-level" type="text"/></td>
+					<td><input name="mulai_input[0]" class="input-block-level" type="date"/></td>
+					<td><input name="akhir_input[0]" class="input-block-level" type="date" /></td>
 					<td></td>
 				</tr>
 				</tbody>
 				<tfoot>
 				<tr>
+					<td></td>
+					<td></td>
 					<td></td>
 					<td></td>
 					<td>
@@ -66,11 +72,22 @@
 		<p>
 			<?php
 			if (isset($_POST['submit'])) {
-				$jenis = $_POST['jenis_input'];
-				$jumlah = $_POST['jumlah_input'];
+				$nama = $_POST['nama_input'];
+				$tempat = $_POST['tempat_input'];
+				$mulai = $_POST['mulai_input'];
+				$akhir = $_POST['akhir_input'];
 
-				foreach ($jenis as $key => $j) {
-					echo "<p>" . $j . " : " . $jumlah[$key] . "</p>";
+				foreach ($nama as $key => $j) {
+					echo "<p>" . $j . "</p>";
+				}
+				foreach ($tempat as $key => $j) {
+					echo "<p>" . $j . "</p>";
+				}
+				foreach ($mulai as $key => $j) {
+					echo "<p>" . $j . "</p>";
+				}
+				foreach ($akhir as $key => $j) {
+					echo "<p>" . $j . "</p>";
 				}
 			}
 			?>
@@ -85,30 +102,48 @@
 
 //                membuat element
 		var row = document.createElement('tr');
-		var jenis = document.createElement('td');
-		var jumlah = document.createElement('td');
+		var nama = document.createElement('td');
+		var tempat = document.createElement('td');
+		var mulai = document.createElement('td');
+		var akhir = document.createElement('td');
 		var aksi = document.createElement('td');
 
 //                meng append element
 		itemlist.appendChild(row);
-		row.appendChild(jenis);
-		row.appendChild(jumlah);
+		row.appendChild(nama);
+		row.appendChild(tempat);
+		row.appendChild(mulai);
+		row.appendChild(akhir);
 		row.appendChild(aksi);
 
 //                membuat element input
-		var jenis_input = document.createElement('input');
-		jenis_input.setAttribute('name', 'jenis_input[' + i + ']');
-		jenis_input.setAttribute('class', 'input-block-level');
+		var nama_input = document.createElement('input');
+		nama_input.setAttribute('name', 'nama_input[' + i + ']');
+		nama_input.setAttribute('class', 'input-block-level');
+		nama_input.setAttribute('type', 'text');
 
-		var jumlah_input = document.createElement('input');
-		jumlah_input.setAttribute('name', 'jumlah_input[' + i + ']');
-		jumlah_input.setAttribute('class', 'input-block-level');
+		var tempat_input = document.createElement('input');
+		tempat_input.setAttribute('name', 'tempat_input[' + i + ']');
+		tempat_input.setAttribute('class', 'input-block-level');
+		tempat_input.setAttribute('type', 'text');
+
+		var mulai_input = document.createElement('input');
+		mulai_input.setAttribute('name', 'mulai_input[' + i + ']');
+		mulai_input.setAttribute('class', 'input-block-level');
+		mulai_input.setAttribute('type', 'date');
+
+		var akhir_input = document.createElement('input');
+		akhir_input.setAttribute('name', 'akhir_input[' + i + ']');
+		akhir_input.setAttribute('class', 'input-block-level');
+		akhir_input.setAttribute('type', 'date');
 
 		var hapus = document.createElement('span');
 
 //                meng append element input
-		jenis.appendChild(jenis_input);
-		jumlah.appendChild(jumlah_input);
+		nama.appendChild(nama_input);
+		tempat.appendChild(tempat_input);
+		mulai.appendChild(mulai_input);
+		akhir.appendChild(akhir_input);
 		aksi.appendChild(hapus);
 
 		hapus.innerHTML = '<button class="btn btn-small btn-default"><i class="fas fa-trash-alt"></i></button>';
