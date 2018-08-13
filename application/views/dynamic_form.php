0a5a679a2c8c3d9fa764e04bc68a3655fe3a3eb6
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-08-13 13:33:35 +0700

    form dinamis in testing

--- application/views/dynamic_form.php
+++ application/views/dynamic_form.php
@@ -11,7 +11,7 @@
 <head>
 	<title>Dinamic Form - harviacode.com</title>
 	<style>
-		body{
+		body
 			padding: 15px
 		}
 		input[type="text"]{
@@ -30,34 +30,130 @@
 <div class="row-fluid">
 	<div class="span6">
 		<form action="#" method="post">
-			<table class="table table-responsive	">
-				<thead>
+			<table class="table table-responsive">
+				<!-- BAGIAN SURAT YANG TIDAK BERUBAH -->
 				<tr>
-					<th>Nama</th>
+					<th>Nomor Surat</th>
+					<th>Jenis </th>
+					<th>Opsi</th>
+				</tr>
+				<tbody>
+				<tr>
+					<td><input name="nomor" class="input-block-level" type="text"/></td>
+					<td><input
+							id="jenisPembayaran"
+							name="jenisPembayaran"
+							onClick="bayar()"
+							type="checkbox" value="0" /> Bayar di depan ? <br /></td>
+					<td><input
+							id="check"
+							name="check"
+							onClick="toggle('check', 'labelitemlist', 'itemlist')"
+							type="checkbox" value="0" /> Banyak tempat ? <br /></td>
+				</tr>
+				</tbody>
+			</table>
+			<table class="table table-responsive">
+				<!-- BAGIAN INI HANYA MUNCUL JIKA OPSI BANYAK TEMPAT TIDAK DI PILIH. -->
+				<tbody id="itemgeneral" style="display: block">
+				<tr>
+					<td><label>Nama Kegiatan</label><br/><input name="kegiatan" class="input-block-level" type="text"/></td>
+					<td><label>Tanggal Mulai</label><br/><input name="mulai" class="input-block-level" type="date"/></td>
+					<td><label>Tanggal Akhir</label><br/><input name="akhir" class="input-block-level" type="date" /></td>
+					<td style="display: block" id="moxspoy">
+						<label>Nama Pegawai yang di tugaskan</label><br/>
+						<select multiple="multiple" id="moxs"  name="my-select[]">
+							<?php foreach ($pegawai as $row) {
+								echo
+								"<option value='$row->id_pegawai'>$row->nama_pegawai</option>" ;
+							}
+							?>
+						</select></td>
+					<td><label>Nama Tempat</label><br/><input name="tempat" class="input-block-level" type="text"/></td>
+				</tr>
+				<tr>
+					<td>
+						<label >Uang Harian: </label>
+						<div class="input-group">
+							<select name="my-select-harian[]">
+								<?php foreach ($harian as $row) {
+									echo
+									"<option value='$row->luar_kota'>$row->provinsi</option>" ;
+								}
+								?>
+							</select>
+						</div>
+					</td>
+					<td>
+						<label >Uang Penginapan: </label>
+						<div class="input-group">
+							<select name="my-select-penginapan[]">
+								<?php foreach ($penginapan as $row) {
+									echo
+									"<option value='$row->eselon_4'>$row->provinsi</option>" ;
+								}
+								?>
+							</select>
+						</div>
+					</td>
+					<td>
+						<label >Tiket Pesawat: </label>
+						<div class="input-group">
+							<select name="my-select-tiket[]">
+								<?php foreach ($tiket as $row) {
+									echo
+									"<option value='$row->biaya_tiket'>$row->rute</option>" ;
+								}
+								?>
+							</select>
+						</div>
+					</td>
+					<td>
+						<label >Uang Transportasi: </label>
+						<div class="input-group">
+							<select name="my-select-transport[]">
+								<?php foreach ($transport as $row) {
+									echo
+									"<option value='$row->besaran'>$row->provinsi</option>" ;
+								}
+								?>
+							</select>
+						</div>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+
+			<table class="table table-responsive">
+				<!-- BAGIAN FORM DINAMIS INI AKAN MUNCUL JIKA USER MENGKLIK OPSI BANYAK TEMPAT-->
+				<thead id="labelitemlist" style="display: none">
+				<tr>
 					<th>Tempat</th>
 					<th>Tgl Mulai</th>
 					<th>Tgl Akhir</th>
-					<th></th>
+					<th>Nama Pegawai yang ditugaskan</th>
 				</tr>
 				</thead>
+
 				<!--elemet sebagai target append-->
-				<tbody id="itemlist">
+				<tbody id="itemlist" style="display: none;">
 				<tr>
-					<td><input name="nama_input[0]" class="input-block-level" type="text"/></td>
 					<td><input name="tempat_input[0]" class="input-block-level" type="text"/></td>
 					<td><input name="mulai_input[0]" class="input-block-level" type="date"/></td>
 					<td><input name="akhir_input[0]" class="input-block-level" type="date" /></td>
-					<td></td>
+					<td><select  name="my-select[0]">
+							<?php foreach ($pegawai as $row) {
+								echo
+								"<option value='$row->id_pegawai'>$row->nama_pegawai</option>" ;
+							}
+							?>
+						</select></td>
 				</tr>
 				</tbody>
 				<tfoot>
 				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
 					<td>
-						<button class="btn btn-small btn-default" onclick="additem(); return false"><i class="fas fa-plus-square"></i></i></button>
+						<button class="btn btn-small btn-default" id="btn_tambah" disabled onclick="additem(); return false"><i class="fas fa-plus-square"></i></i></button>
 						<button name="submit" class="btn btn-small btn-primary"><i class="fas fa-check-circle"></i></button>
 					</td>
 				</tr>
@@ -98,6 +194,7 @@
 	</div>
 </div>
 <script>
+
 	var i = 1;
 	function additem() {
 //                menentukan target append
@@ -105,25 +202,19 @@
 
 //                membuat element
 		var row = document.createElement('tr');
-		var nama = document.createElement('td');
 		var tempat = document.createElement('td');
 		var mulai = document.createElement('td');
 		var akhir = document.createElement('td');
+		var pegawai = document.createElement('td');
 		var aksi = document.createElement('td');
 
 //                meng append element
 		itemlist.appendChild(row);
-		row.appendChild(nama);
 		row.appendChild(tempat);
 		row.appendChild(mulai);
 		row.appendChild(akhir);
+		row.appendChild(pegawai);
 		row.appendChild(aksi);
-
-//                membuat element input
-		var nama_input = document.createElement('input');
-		nama_input.setAttribute('name', 'nama_input[' + i + ']');
-		nama_input.setAttribute('class', 'input-block-level');
-		nama_input.setAttribute('type', 'text');
 
 		var tempat_input = document.createElement('input');
 		tempat_input.setAttribute('name', 'tempat_input[' + i + ']');
@@ -140,13 +231,25 @@
 		akhir_input.setAttribute('class', 'input-block-level');
 		akhir_input.setAttribute('type', 'date');
 
+
+		var pegawai_input = document.createElement('select');
+		pegawai_input.setAttribute('name', 'my-select['+ i +']');
+
+		<?php foreach ($pegawai as $row) { ?>
+		var option_pegawai = document.createElement('option');
+		option_pegawai.setAttribute('value', '<?php echo $row->id_pegawai?>' );
+		option_pegawai.innerHTML = "<?php echo $row->nama_pegawai ?>";
+		pegawai_input.appendChild(option_pegawai);
+		<?php }
+		?>
+
 		var hapus = document.createElement('span');
 
 //                meng append element input
-		nama.appendChild(nama_input);
 		tempat.appendChild(tempat_input);
 		mulai.appendChild(mulai_input);
 		akhir.appendChild(akhir_input);
+		pegawai.appendChild(pegawai_input);
 		aksi.appendChild(hapus);
 
 		hapus.innerHTML = '<button class="btn btn-small btn-default"><i class="fas fa-trash-alt"></i></button>';
@@ -157,6 +260,32 @@
 
 		i++;
 	}
+
+	//Button tambah ga bisa di klik kalo opsi Banyak tempat tidak di ceklis
+	function toggle(checkboxID, labelID, inputID) {
+		var checkbox = document.getElementById(checkboxID);
+		var label = document.getElementById(labelID);
+		var input = document.getElementById(inputID);
+		var btntambah = document.getElementById('btn_tambah');
+		var itemgeneral = document.getElementById('itemgeneral');
+		if(checkbox.checked) {
+			label.style.display = "block";
+			input.style.display = "block";
+			btntambah.disabled = false;
+			itemgeneral.style.display = "none";
+
+		} else {
+			label.style.display = "none";
+			input.style.display = "none";
+			btntambah.disabled = true;
+			itemgeneral.style.display = "block";
+		}
+		//updateToggle = checkbox.checked ? toggle.disabled=false : toggle.disabled=true;
+		//updateSelect = checkbox.checked ? select_pegawai.disabled=true : select_pegawai.disabled=false;
+	}
+
+	$('#moxs').multiSelect();
+
 </script>
 </body>
 </html>
