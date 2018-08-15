8989d072f8d341ca2af54705f69b781862a12092
Erik Santiago <eriksantiago.science@gmail.com>
2018-08-15 15:52:01 +0700

    add session lin at autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'pdf');
+$autoload['libraries'] = array('database', 'pdf', 'session');
 
 /*
 | -------------------------------------------------------------------
