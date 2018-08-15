d1c17f91a479487998a62e6da1a64078bb707c31
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-08-15 14:27:16 +0700

    fix surat hilang with backend

--- application/controllers/C_PDF.php
+++ application/controllers/C_PDF.php
@@ -671,8 +671,8 @@ class C_PDF extends CI_Controller {
 		$rute			= $tiket_result['0']->rute;
 
 		//get real pengeluaran untuk tiket
-
-		$r_tiket =
+		$r_tiket_result = $this->db->get_where('spd_rampung', array('id_surat' => $arr_slug[0], 'id_pegawai' => $arr_slug[1]))->result();
+		$r_tiket = $r_tiket_result['0']->tiket;
 
 		$ppk 			= $this->db->get_where('pejabat_administratif',
 			array('jabatan' => 'Pejabat Pembuat Komitmen'))->result();
