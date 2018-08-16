a6624272bbd4bd840079d243f9865bfa81826c0a
Kelik Is Cahyanto <kelikisc@gmail.com>
2018-08-16 13:07:14 +0700

    delete link footer

--- application/views/layouts/footer2.php
+++ application/views/layouts/footer2.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="page-footer fixed-bottom font-small unique-color-dark">
-  <div class="footer-copyright py-3" align="center">© 2018 Copyright: <a href="https://instagram.com/kelikisc">.Native
+  <div class="footer-copyright py-3" align="center">© 2018 Copyright: <a href="">.Native
   </div>
 </footer>
 <!-- Footer -->
