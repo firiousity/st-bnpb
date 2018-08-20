b674bdd82912118fac2bdd1e1e153ea97cb72a1f
Kelik Is Cahyanto <kelikisc@gmail.com>
2018-08-20 12:04:26 +0700

    gatau surat apa

--- application/controllers/C_PDF.php
+++ application/controllers/C_PDF.php
@@ -1424,7 +1424,7 @@ class C_PDF extends CI_Controller {
 			$pdf->Cell(10,6,$hari.' Hari',0,0,'L',0);
 			$pdf->Cell(10,6,'x',0,0,'R',0);
 			$pdf->Cell(5,6,'Rp',0,0,'L',0);
-			$pdf->Cell(25,6,$harian,'R',0,'R',0);
+			$pdf->Cell(25,6,number_format($harian,2,',','.'),'R',0,'R',0);
 			$pdf->Cell(10,6,'Rp',0,0,'L',0);
 			$pdf->Cell(30,6,$total_harian,'R',0,'R',0);
 			$pdf->Cell(55,6,'Perjalanan dinas ke :','R',0,'L',0);
@@ -1432,7 +1432,7 @@ class C_PDF extends CI_Controller {
 			$pdf->Cell(5,7,'',0,0);
 			$pdf->Cell(10,6,'2','L',0,'C',0);
 			$pdf->Cell(25,6,'Penginapan','L',0,'L',0);
-			$pdf->Cell(10,6,$malam.' Malam',0,0,'L',0);
+			$pdf->Cell(10,6,number_format($malam,2,',','.'),' Malam',0,0,'L',0);
 			$pdf->Cell(10,6,'x',0,0,'R',0);
 			$pdf->Cell(5,6,'Rp',0,0,'L',0);
 			$pdf->Cell(25,6,$penginapan,'R',0,'R',0);
