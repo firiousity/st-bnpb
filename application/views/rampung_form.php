7e4e78dda6e59a0ed5abc66709cdd364ba35e822
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-08-21 08:53:15 +0700

    add dynamic input in rampung form. backend needed

--- application/views/rampung_form.php
+++ application/views/rampung_form.php
@@ -12,25 +12,72 @@
 				  action="<?php echo base_url('C_PDF/print_rampung/'.$slug) ?>"
 				  method="post">
 				<h2 class="judul">ISIAN FORM RAMPUNG</h2>
-				<div class="form-row">
-					<div class="col-md-4 mb-3">
-						<label for="validationCustom02">Uang Penginapan</label>
-						<input type="number" class="form-control" id="validationCustom02" required name="penginapan"
-							   placeholder="Rp. " required>
-
-					</div>
-					<div class="col-md-4 mb-3">
-						<label for="validationCustom02">Tiket Pesawat</label>
-						<input type="number" class="form-control" id="validationCustom02" required name="tiket"
-							   placeholder="Rp. " required>
-
-					</div>
-				</div></div>
+				<table class="table table-responsive" width="100%">
+					<!-- BAGIAN FORM DINAMIS INI AKAN MUNCUL JIKA USER MENGKLIK OPSI BANYAK TEMPAT-->
+					<!--elemet sebagai target append-->
+					<tbody id="itemlist">
+					<tr>
+						<td>
+							<label for="validationCustom02">Uang Penginapan</label>
+							<input type="number" class="form-control" id="validationCustom02" required name="penginapan"
+								   placeholder="Rp. " required>
+						</td>
+						<td>
+							<label for="validationCustom02">Tiket Pesawat</label>
+							<input type="number" class="form-control" id="validationCustom02" required name="tiket"
+								   placeholder="Rp. " required></td>
+					</tr>
+					<tr>
+						<td>
+							<button class="btn btn-blue" id="btn_tambah" onclick="additem(); return false"><i class="fas fa-plus-square"></i></i></button>
+						</td>
+					</tr>
+					</tbody>
+				</table>
 
 				<button class="btn btn-indigo" type="submit" name="rsubmit">Print Surat</button>
 			</form>
 		</div>
 
 	</div>
+<script>
+	var i = 1;
+	function additem() {
+//                menentukan target append
+		var itemlist = document.getElementById('itemlist');
+
+//                membuat element
+		var row = document.createElement('tr');
+		var penginapan = document.createElement('td');
+		var aksi = document.createElement('td');
+
+//                meng append element
+		itemlist.appendChild(row);
+		row.appendChild(penginapan);
+		row.appendChild(aksi);
+
+
+		var penginapan_input = document.createElement('input');
+		penginapan_input.setAttribute('name', 'penginapan['+ i +']');
+		penginapan_input.setAttribute('type', 'number');
+		penginapan_input.setAttribute('class', 'form-control');
+
+
+		var hapus = document.createElement('span');
+
+//                meng append element input
+		penginapan.appendChild(penginapan_input);
+		aksi.appendChild(hapus);
+
+		hapus.innerHTML = '<button class="btn btn-small btn-default"><i class="fas fa-trash-alt"></i></button>';
+//                membuat aksi delete element
+		hapus.onclick = function () {
+			row.parentNode.removeChild(row);
+		};
+
+		i++;
+	}
+
+</script>
 </body>
 
